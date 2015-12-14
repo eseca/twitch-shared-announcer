058c1416ef36bc3943b9f7ce69f9fc575d3d07f0
sk <sk.valshk@gmail.com>
2015-12-13 20:58:10 -0600

    First version

--- assets/js/hud.js
+++ assets/js/hud.js
@@ -1,54 +1,57 @@
-var schedules = [
-{ 
-    'type': 'twitch',
-    'image': '',
-    'title': 'Krankes_Hirn',
-    'description': 'Miércoles y Jueves, 10pm GMT-5'
-},
-{ 
-    'type': 'youtube',
-    'image': '',
-    'title': 'El gordo criticón',
-    'description': 'Video ensayos sobre videojuegos'
-},
-{ 
-    'type': 'twitch',
-    'image': '',
-    'title': 'LTPhantom',
-    'description': 'Lunes, Martes, ~10pm GMT-5'
-},
-]
+var announcerApp = angular.module('announcerApp', ['ngAnimate']);
 
-var typeContainer = document.getElementById('type');
-var titleContainer = document.getElementById('reminderTitle');
-var descContainer = document.getElementById('reminderDescription');
+announcerApp.controller('HudCtrl', ['$scope', '$interval', function($scope, $interval) {
+    console.log("Loading HudCtrl...");
+    $scope.animation_class = 'hidden';
+    $scope.i = 0;
+    $scope.current = {};
+    $scope.announcements = [
+        {
+            "type": "series",
+            "title": "Camino a Mighty no. 9",
+            "image": "assets/img/road_to_mighty_no_9/beck.png",
+            "text": "Sábados 16hrs GMT-6. A partir del 26 de Diciembre"
+        },
+        {
+            "type": "twitch",
+            "title": "Pranzedd",
+            "channel": "pranzedd",
+            "image": "assets/img/twitchLogo.png",
+            "text": "Una de las comunidades más grandes y amenas de Twitch"
+        },
+        {
+            "type": "twitch",
+            "channel": "krankes_hirn",
+            "title": "Krankes Hirn, El Gordo Criticón",
+            "image": "assets/img/twitchLogo.png",
+            "text": "Quejándose de los juegos, en vivo"
+        },
+        {
+            "type": "twitch",
+            "title": "¡La mejor variedad de juegos en Twitch!",
+            "image": "assets/img/gaminglat.png",
+            "text": "No olvides revisar la programación"
+        },
+    ];
 
+    $scope.fetchInfo = function() {
+        $scope.current = $scope.announcements[$scope.i];
+        console.log($scope.current);
+        $scope.i = ($scope.i+1) % $scope.announcements.length;
+    };
 
-var c = 0;
-function displayHUD() {
-    console.log('displayHUD');
+    $interval($scope.fetchInfo, 60000);
+}]);
 
-    typeContainer.innerHTML = schedules[c].type;
-    titleContainer.innerHTML = schedules[c].title;
-    descContainer.innerHTML = schedules[c].description;
-
-    e = document.getElementById('reminderContainer');
-
-    // -> removing the class
-    e.classList.remove("reminderContainer");
-
-    // -> triggering reflow /* The actual magic */
-    // without this it wouldn't work. Try uncommenting the line and the transition won't be retriggered.
-    e.offsetWidth = e.offsetWidth;
-
-    // -> and re-adding the class
-    e.classList.add("reminderContainer");
-
-    c = (c + 1) % schedules.length;
-}
-
-// FIXME: This is kind of noob
-displayHUD();
-
-window.setInterval(displayHUD, 15000);
-
+announcerApp.directive('animateOnChange', function($animate,$timeout) {
+    return function(scope, elem, attr) {
+        scope.$watch(attr.animateOnChange, function(nv,ov) {
+            if (nv!=ov) {
+                var c = 'change';
+                $animate.addClass(elem,c).then(function() {
+                    $timeout(function() {$animate.removeClass(elem,c);});
+                });
+            }
+        });
+    };
+});
